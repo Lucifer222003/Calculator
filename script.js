5eea3d2263f6071e9db1303635ce06c3612b3d11
Kirill Moskalyov <330_350@mail.ru>
2022-04-16 02:54:59 +0600

    lesson02

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "MyProject";
+console.log(typeof title)
+let screens = "Large, Medium, Small"
+console.log(screens.length)
+let screenPrice = "20";
+console.log(typeof screenPrice)
+let rollback = "50";
+let fullPrice = "100";
+console.log(typeof fullPrice);
+let adaptive = true;
+console.log(typeof adaptive)
 
+console.log(screenPrice.toUpperCase())
+console.log(fullPrice.toUpperCase())
 
-alert('привет мир');
-console.log('создал ветку lesson01');
+console.log(screens.toLowerCase())
+let razbitStrokuNaMassiv = screens.split(',');
+console.log(razbitStrokuNaMassiv);
+
+console.log(fullPrice * (rollback/100))
