896a8371cf976f5a127e8cdd2b1ff0e164e55c8b
Kirill Moskalyov <330_350@mail.ru>
2022-04-16 03:34:50 +0600

    fix lessons02

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 let title = "MyProject";
-console.log(typeof title)
 let screens = "Large, Medium, Small"
-console.log(screens.length)
-let screenPrice = "20";
-console.log(typeof screenPrice)
-let rollback = "50";
-let fullPrice = "100";
-console.log(typeof fullPrice);
+let screenPrice = 20;
+let rollback = 50; 
+let fullPrice = 100;
 let adaptive = true;
+
+console.log(typeof title)
+console.log(typeof fullPrice);
 console.log(typeof adaptive)
-
-console.log(screenPrice.toUpperCase())
-console.log(fullPrice.toUpperCase())
-
-console.log(screens.toLowerCase())
-let razbitStrokuNaMassiv = screens.split(',');
-console.log(razbitStrokuNaMassiv);
-
+console.log(screens.length)
+console.log("Стоимость верстки экранов " + screenPrice  +  " рублей")
+console.log("Стоимость разработки сайтов " + fullPrice  +  " рублей")
+console.log(screens.toLowerCase().split(', '));
 console.log(fullPrice * (rollback/100))
